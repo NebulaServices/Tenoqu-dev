232ec388aced4132cb4b316ce0f3af3b881e676a
830008 <nrollins189@gmail.com>
2022-12-26 19:06:39 -0600

    Add Static

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'));
 app.listen(port, () => console.log(`Tenoqu listening on port ${port}!`));
