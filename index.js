734b1e379acd424269d9e4665019a045fc5f62ef
830008 <nrollins189@gmail.com>
2022-12-26 19:26:19 -0600

    Change port again lol

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 3000;
 app.use(express.static('public'));
 app.listen(port, () => console.log(`Tenoqu listening on port ${port}!`));
