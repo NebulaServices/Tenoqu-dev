1f6bb0abd7ada9c50f51db21d885e4f3e323437c
830008 <nrollins189@gmail.com>
2022-12-26 18:42:07 -0600

    Setup

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Tenoqu listening on port ${port}!`));
