4f4cdae14a865190e1aa4128f6d391d982983ce8
830008 <nrollins189@gmail.com>
2022-12-26 21:34:14 -0600

    Fix Typo #3

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('public/appindex'));
+app.get('/', (req, res) => res.render('public/app/index'));
 app.listen(port, () => console.log(`Tenoqu listening on port ${port}!`));
