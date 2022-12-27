47751eceea63eebb4111a2dca7237adf7814da68
830008 <nrollins189@gmail.com>
2022-12-26 21:32:17 -0600

    Add Ejs Rendering

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.get/('/', (req, res) => res.render('/public/appindex'));
 app.listen(port, () => console.log(`Tenoqu listening on port ${port}!`));
