952fe8f5569e73ef3f3993681d0cb36b93b8b5be
Linux Snoot <49733954+NovaAppsInc@users.noreply.github.com>
2023-01-02 21:25:54 -0600

    half server fix

--- index.js
+++ index.js
@@ -3,20 +3,28 @@ const path = require('path')
 const dotenv = require('dotenv')
 dotenv.config()
 
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'res'),
+  prefix: '/balla/',
+});
+
 fastify.register(require("@fastify/view"), {
-    engine: {
-      ejs: require("ejs"),
-    },
-  });
-  fastify.get('/', async (request, reply) => {
-    return reply.view("/views/index", { title: "Tenoqu" });
-    });
-  fastify.get('*', async (request, reply) => {
-    return reply.view("/views/"+request.url, { title: "Tenoqu" });
-    });
+  engine: {
+    ejs: require("ejs"),
+  },
+});
+
+fastify.get('/', async (request, reply) => {
+  return reply.view("/views/index", { title: "Tenoqu" });
+});
+
+fastify.get('*', async (request, reply) => {
+  return reply.view("/views/"+request.url, { title: "Tenoqu" });
+});
+
 fastify.listen({port: process.env.PORT, host: '0.0.0.0'}, (err, address) => {
-    if (err) throw err
-    fastify.log.info(`Tenoqu listening on ${address}`)
-    console.log("Tenoqu is running on port "+process.env.PORT+"!")
-    })
+  if (err) throw err
+  fastify.log.info(`Tenoqu listening on ${address}`)
+  console.log("Tenoqu is running on port "+process.env.PORT+"!")
+})
     
